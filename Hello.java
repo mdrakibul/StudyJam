0d714e43533e837ddbd08665f0aef22633ee3809
mdrakibul <mdrakibul@live.com>
2015-08-23 17:03:21 +0600

    This is the second version

--- Hello.java
+++ Hello.java
@@ -1,5 +1,14 @@
 class Hello{
-	public static void main(String args[]){
+	public static void main(String args[]) {
 		System.out.println("Hello GoogleStudyJam");
+
+		New ob = new New();
+		ob.Print();
+	}
+}
+
+class New{
+	public void Print() {
+		System.out.println("New method");
 	}
 }
